3bbc9499284205e82d3ef5a00dec4b78fbed775d
pese2912 <pese2912@naver.com>
2016-04-01 18:36:50 +0000

    lotto

---
+++ app/helpers/random_controller_helper.rb
@@ -0,0 +1,2 @@
+module RandomControllerHelper
+end
---
+++ test/controllers/random_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RandomControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
